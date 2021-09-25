2cb2c1bb693fe72fd4084733dcd669f01db3d81a
Gab629 <gab.gauthier2002@gmail.com>
2021-09-25 13:48:49 -0400

    Initialisation du header

---
+++ src/scripts/ComponentFactory.js
@@ -0,0 +1,31 @@
+import Header from './components/Header';
+import Scrolly from './components/Scrolly';
+import Form from './components/Form';
+
+export default class ComponentFactory {
+  constructor() {
+    this.componentInstances = [];
+    this.componentList = {
+      Header,
+      Scrolly,
+      Form,
+    };
+    this.init();
+  }
+
+  init() {
+    const components = document.querySelectorAll('[data-component]');
+
+    for (let i = 0; i < components.length; i++) {
+      const element = components[i];
+      const componentName = element.dataset.component;
+
+      if (this.componentList[componentName]) {
+        const instance = new this.componentList[componentName](element);
+        this.componentInstances.push(instance);
+      } else {
+        console.log(`La composante ${componentName} n'existe pas`);
+      }
+    }
+  }
+}
---
+++ src/scripts/components/Form.js
@@ -0,0 +1,85 @@
+/** Composante form de TimTools */
+export default class form {
+  /**
+   * Méthode constructeur
+   * @param {HTMLElement} element - Élément HTML sur lequel la composante est instanciée
+   */
+  constructor(element) {
+    this.element = element;
+    this.formElements = this.element.elements;
+    this.init();
+  }
+
+  /**
+   * Méthode d'initialisation
+   */
+  init() {
+    this.element.setAttribute('novalidate', '');
+
+    for (let i = 0; i < this.formElements.length; i++) {
+      const input = this.formElements[i];
+
+      if (input.required) {
+        input.addEventListener('input', this.validateInput.bind(this));
+      }
+    }
+
+    this.element.addEventListener('submit', this.onSubmit.bind(this));
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+
+    if (this.validate()) {
+      console.log('succes');
+      // envoie ajax du formulaire
+
+      this.showConfirmation();
+    } else {
+      console.log('error');
+    }
+  }
+
+  /**
+   * Méthode description
+   * @return {boolean} status de la validation
+   */
+  validate() {
+    let isValid = true;
+    for (let i = 0; i < this.formElements.length; i++) {
+      const input = this.formElements[i];
+
+      if (input.required && !this.validateInput(input)) {
+        isValid = false;
+      }
+    }
+
+    return isValid;
+  }
+
+  validateInput(event) {
+    const input = event.currentTarget || event;
+
+    if (input.validity.valid) {
+      this.removeError(input);
+    } else {
+      this.addError(input);
+    }
+
+    return input.validity.valid;
+  }
+
+  addError(input) {
+    const container = input.closest('[data-input-container]') || input.closest('.input');
+    container.classList.add('error');
+  }
+
+  removeError(input) {
+    const container = input.closest('[data-input-container]') || input.closest('.input');
+    container.classList.remove('error');
+  }
+
+  showConfirmation() {
+    this.element.classList.add('is-sent');
+  }
+}
