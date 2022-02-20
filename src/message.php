fec74c0d59962e29c75201dbdd91e9554acceba3
Gabriel Gauthier <gab.gauthier2002@gmail.com>
2022-02-20 15:44:42 -0500

    Page contact amélioré

--- src/message.php
+++ src/message.php
@@ -7,7 +7,7 @@
     $subject = "From: $name <$email>";
     $body = "Name: $name\nEmail: $email\nPhone: $phone\nMessage:\n$comment\n\nRegards,\n$name";
     mail("gab.gauthier2002@gmail.com",$subject,$body);
-    header("Location: https://ggauthier.dectim.ca/contact.html");
+    header("Location: https://ggauthier.ca/contact.html");
 
   }else{
     echo "Email and message field is required!";
