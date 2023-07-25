6911f81c939cb191d693cfcfd1e5d59b9e8efd85
Gabriel Gauthier <gab.gauthier2002@gmail.com>
2023-07-25 11:36:09 -0400

    MAJ portfolio - Latency + NinwOClock

--- src/message.php
+++ src/message.php
@@ -2,13 +2,12 @@
   $name = htmlspecialchars($_GET['fullname']);
   $email = htmlspecialchars($_GET['email']);
   $phone = htmlspecialchars($_GET['phone']);
-  $comment = htmlspecialchars($_GET['comment']);
+  $comment = $_GET['comment']; // Removed htmlspecialchars here
   if(!empty($email) && !empty($comment)){
     $subject = "From: $name <$email>";
     $body = "Name: $name\nEmail: $email\nPhone: $phone\nMessage:\n$comment\n\nRegards,\n$name";
     mail("gab.gauthier2002@gmail.com",$subject,$body);
     header("Location: https://ggauthier.ca/contact.html");
-
   }else{
     echo "Email and message field is required!";
   }
